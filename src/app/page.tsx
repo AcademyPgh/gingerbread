254c2b37074ba0dc1f76ca1281c3918146ed4565
Ed <ethereos@gmail.com>
2023-06-21 15:49:36 -0400

    used drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
-import { connect } from '@planetscale/database'
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { InferModel } from 'drizzle-orm'; 
 
-const config = {
-  //add connection info here
+const connection = connect({
   host: process.env["DATABASE_HOST"],
   username: process.env["DATABASE_USERNAME"],
-  password: process.env["DATABASE_PASSWORD"]
-}
+  password: process.env["DATABASE_PASSWORD"],
+});
 
-const conn = connect(config)
-const results = await conn.execute('SELECT name FROM runners;')
+export const users = pgTable('runners', {
+  id: serial('id').primaryKey(),
+  name: text('name'),
+});
 
+type User = InferModel<typeof users, "select">;
+type NewUser = InferModel<typeof users, "insert">;
+
+const db = drizzle(connection);
+const result: User[] = await db.select().from(users);
+console.log(result[0].id,result[0].name);
 export default function Home() {
-  
-  const rows = results["rows"];
-  const stuff = JSON.stringify(rows);
+  const output = result.map((_,index) => <div>{_.name}</div>);
   return (
     <div>
       <div>HELL YES</div>
-      {stuff}
+      {output}
     </div>
   )
 }
